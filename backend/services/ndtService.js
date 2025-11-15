40a3c05786c4d8061d0ece40cf37841d8666e31a
Arthur Marques Azevedo <arthurmarquesazevedo@gmail.com>
2025-11-15 12:31:27 -0300

    fix: ajusta cálculos de PBvd, EEvd e CNFvd na função calculateNdt

--- backend/services/ndtService.js
+++ backend/services/ndtService.js
@@ -6,15 +6,15 @@ function calculateNdt(input) {
 
     let PBvd;
     if (input.PIDN == 0.0 && input.PIDA == 0.0) {
-        PBvd = C.PBvd.NO_PIDN_PIDA_FACTOR * input.PB;
+        PBvd = (C.PBvd.NO_PIDN_PIDA_FACTOR * input.PB) - 0.44;
     } else {
         PBvd = C.PBvd.FACTOR1 * (input.PB - input.PIDN) + C.PBvd.FACTOR2 * (input.PIDN - input.PIDA);
     }
 
     const CNF = input.MO - input.EE - input.PB - input.FDN;
-    const EEvd = C.EEvd_FACTOR * input.EE;
+    const EEvd = (C.EEvd_FACTOR * input.EE) - 0.33;
     const FDNd = C.FDNd_FACTOR * (input.FDN - input.Ligrina) * (1 - (input.Ligrina) / (input.FDN ** C.FDNd_EXP));
-    const CNFvd = C.CNFvd_FACTOR * CNF;
+    const CNFvd = (C.CNFvd_FACTOR * CNF) - 3.22;
     const NDT_Result = PBvd + EEvd * 2.25 + FDNd + CNFvd - C.NDT_SUB;
     const Ed = NDT_Result * C.ED_FACTOR;
     const Em_caprinos = C.EM_CAPRINOS.BASE + C.EM_CAPRINOS.FACTOR * Ed;
